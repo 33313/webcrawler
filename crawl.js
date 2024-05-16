076e40ef301f27925ce8384fd5ed08f8116c5130
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-05-16 18:58:37 +0200

    refactor: use WHATWG API for URL normalization

--- crawl.js
+++ crawl.js
@@ -1,5 +1,7 @@
-function normalizeURL(url) {
-    return url.replace(/^https?:\/\//, "").replace(/\/$/, "")
+function normalizeURL(urlString) {
+    const url = new URL(urlString)
+    let path = `${url.host}${url.pathname}`
+    return path.replace(/^https?:\/\//, "").replace(/\/$/, "")
 }
 
 export { normalizeURL }
