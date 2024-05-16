13fc3aade59de1df99bf89ba610aa5e3566d53d5
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-05-16 20:23:08 +0200

    feat: get all URLs from HTML body

--- crawl.js
+++ crawl.js
@@ -1,7 +1,26 @@
+import { JSDOM } from 'jsdom'
+
 function normalizeURL(urlString) {
     const url = new URL(urlString)
     let path = `${url.host}${url.pathname}`
     return path.replace(/^https?:\/\//, "").replace(/\/$/, "")
 }
 
-export { normalizeURL }
+function getURLsFromHTML(htmlBody, baseURL) {
+    const body = new JSDOM(htmlBody)
+    let anchors = body.window.document.querySelectorAll('a')
+    let links = []
+    for (let a of anchors) {
+        let str = a.href
+        if (str.startsWith('/')) {
+            str = `${baseURL}${a.href}`
+        }
+        if (str.endsWith('/')) {
+            str = str.substring(0, str.length - 1)
+        }
+        links.push(str)
+    }
+    return links
+}
+
+export { normalizeURL, getURLsFromHTML }
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,5 @@
 import { test, expect } from "@jest/globals"
-import { normalizeURL } from "./crawl"
-
+import { normalizeURL, getURLsFromHTML } from "./crawl"
 
 test('URL normalization', () => {
     let urls = [
@@ -11,5 +10,29 @@ test('URL normalization', () => {
     ]
     for (let url of urls) {
         expect(normalizeURL(url)).toBe("blog.boot.dev/path")
+    }
+})
+
+test('Get URLs from HTML', () => {
+    const baseURL = "https://example.com"
+    const html = `
+    <html>
+        <body>
+            <a href="${baseURL}"><span>Home</span></a>
+            <a href="/pictures"><span>Pictures</span></a>
+            <a href="/posts"><span>Posts</span></a>
+            <a href="${baseURL}/admin/panel"><span>Admin panel</span></a>
+        </body>
+    </html>
+`
+    const anchors = getURLsFromHTML(html, baseURL)
+    const links = [
+        baseURL,
+        `${baseURL}/pictures`,
+        `${baseURL}/posts`,
+        `${baseURL}/admin/panel`
+    ]
+    for (let i = 0; i < anchors.length; i++) {
+        expect(anchors[i]).toBe(links[i])
     }
 })
